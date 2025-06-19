b5c139bca1f7a6a2d2859601b32c19d31b86b658
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-06-19 10:49:57 +0530

    Create 2294_PartitionArraySuchThatMaximumDifferenceIsK.java

---
+++ Jun_2025/2294_PartitionArraySuchThatMaximumDifferenceIsK.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int ans=1;
+        int min=nums[0];
+
+        for(int i=1;i<nums.length;i++){
+         if(nums[i]-min>k){
+            ans++;
+            min=nums[i];
+         }else{
+            min=Math.min(min,nums[i]);
+         }
+        }
+
+        return ans;
+    }
+}
