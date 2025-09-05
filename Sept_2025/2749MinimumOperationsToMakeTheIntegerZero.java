074c146607a52c73986438a39a4ac8ac7f330349
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-09-06 00:27:16 +0530

    Create 2749_MinimumOperationsToMakeTheIntegerZero.java

---
+++ Sept_2025/2749MinimumOperationsToMakeTheIntegerZero.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int makeTheIntegerZero(int num1, int num2) {
+        int t = 0;
+
+        while (true) {
+            long val = (long) num1 - (long) t * num2;
+
+            if (val < 0)
+                return -1;
+
+            // Integer.bitCount() gives popcount in Java
+            if (Long.bitCount(val) <= t && t <= val) {
+                return t;
+            }
+
+            t++;
+        }
+    }
+}
