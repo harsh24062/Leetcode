48abf201c5b0450ac6c8d67910fd485b167b0564
harsh24062 <129416678+harsh24062@users.noreply.github.com>
2025-10-23 11:47:11 +0530

    Add solution to check if digits are equal in string
    Implement method to check if digits are equal after operations.

---
+++ Oct_2025/3461_CheckIfDigitsAreEqualInStringAfterOperationsI.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasSameDigits(String s) {
+        StringBuilder sb = new StringBuilder(s);
+
+        while(sb.length()!=2){
+            StringBuilder temp = new StringBuilder();
+           for(int i=0;i<sb.length()-1;i++){
+              temp.append(((sb.charAt(i)-'0')+(sb.charAt(i+1)-'0'))%10);
+           }
+           sb=temp;
+        }
+        return (sb.charAt(0)!=sb.charAt(1)?false:true);
+    }
+}
